972e0740435a99d470d0c810948c7b83c546e806
Evgeniy <ewgtut@yandex.ru>
2026-09-11 18:18:43 +0300

    Code style commit#2

--- src/main/ru/practicum/moviehub/api/Endpoint.java
+++ src/main/ru/practicum/moviehub/api/Endpoint.java
@@ -1,3 +1,10 @@
 package ru.practicum.moviehub.api;
 
-public enum Endpoint {GET_MOVIES, GET_MOVIES_FILTERED, DELETE_MOVIE, GET_MOVIE, POST_MOVIE, UNKNOWN}
+public enum Endpoint {
+    GET_MOVIES,
+    GET_MOVIES_FILTERED,
+    DELETE_MOVIE,
+    GET_MOVIE,
+    POST_MOVIE,
+    UNKNOWN
+}
